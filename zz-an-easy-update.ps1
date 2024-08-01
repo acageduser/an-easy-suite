23a554476e56d950baea371b63ed27970509ef92
Ryan Livinghouse <132973297+acageduser@users.noreply.github.com>
2024-08-01 14:21:27 +0900

    Update zz-an-easy-update.ps1

--- zz-an-easy-update.ps1
+++ zz-an-easy-update.ps1
@@ -1,17 +1,5 @@
 # zz-an-easy-update.ps1
 
-# Log file
-$logFile = "an-easy-update.log"
-
-function Write-Log {
-    param (
-        [string]$message
-    )
-    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    $logMessage = "$timestamp - $message"
-    Add-Content -Path $logFile -Value $logMessage
-}
-
 # Ensure Minecraft is closed before running this script
 Write-Host ""
 Write-Host "Before running this script:"
@@ -26,7 +14,6 @@ Write-Host ""
 # Execution policy change
 $originalExecutionPolicy = Get-ExecutionPolicy
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned -Force
-Write-Log "Execution policy set to RemoteSigned."
 
 # Google Drive URLs
 $GDRIVE_MODS_URL = "https://drive.google.com/uc?export=download&id=1LrSgBpQFoLjg_jqv0Tf3dM8isbY--iGJ"
@@ -61,13 +48,11 @@ function Download-File {
         [string]$output
     )
     Write-Host "Downloading $output..."
-    Write-Log "Downloading $output..."
     try {
         Invoke-WebRequest -Uri $url -OutFile $output
-        Write-Log "Downloaded $output successfully."
+        Write-Host "Downloaded $output successfully."
     } catch {
         Write-Host "Download failed for $output. Please check the download link and try again."
-        Write-Log "Download failed for $output: $_"
         exit 1
     }
 }
@@ -75,9 +60,7 @@ function Download-File {
 # Delete the local mods folder before downloading
 if (Test-Path $MODS_FOLDER) {
     Write-Host "Deleting existing mods folder..."
-    Write-Log "Deleting existing mods folder..."
     Remove-Item -Recurse -Force $MODS_FOLDER
-    Write-Log "Existing mods folder deleted."
 }
 
 # Download the mods zip from Google Drive using Invoke-WebRequest
@@ -87,27 +70,18 @@ if (-Not (Test-Path $DOWNLOAD_MODS_FILE)) {
 
 # Check if the download was successful
 if (Test-Path $DOWNLOAD_MODS_FILE) {
-    Write-Log "Download file exists."
-    
     # Extract the downloaded archive using 7-Zip
     Write-Host "Extracting new mods with 7-Zip..."
-    Write-Log "Extracting new mods with 7-Zip..."
     $7zipPath = "C:\Program Files\7-Zip\7z.exe" # Adjust this path if 7-Zip is installed elsewhere
     $extractCommand = "& `"$7zipPath`" x `"$DOWNLOAD_MODS_FILE`" -o`"$MODS_FOLDER`" -y"
     Invoke-Expression $extractCommand
-    Write-Log "Extraction completed."
 
     # Clean up the downloaded archive file
     Write-Host "Cleaning up..."
-    Write-Log "Cleaning up..."
     Remove-Item $DOWNLOAD_MODS_FILE
-    Write-Log "Download file deleted."
-
     Write-Host "Mods update complete!"
-    Write-Log "Mods update complete!"
 } else {
     Write-Host "Download failed. Please check the download link and try again."
-    Write-Log "Download failed. The file does not exist at $DOWNLOAD_MODS_FILE."
 }
 
 # Download the forge-client.toml from Google Drive using Invoke-WebRequest
@@ -117,32 +91,22 @@ if (-Not (Test-Path $DOWNLOAD_CONFIG_FILE)) {
 
 # Check if the download was successful
 if (Test-Path $DOWNLOAD_CONFIG_FILE) {
-    Write-Log "Download file exists."
     # Verify and move the downloaded file
     Write-Host "Deleting existing forge-client.toml..."
-    Write-Log "Deleting existing forge-client.toml..."
     Remove-Item -Force "$CONFIG_FOLDER\forge-client.toml"
-    Write-Log "Existing forge-client.toml deleted."
 
     # Move the downloaded forge-client.toml to the config folder
     Write-Host "Moving new forge-client.toml to the config folder..."
-    Write-Log "Moving new forge-client.toml to the config folder..."
     Move-Item -Force -Path $DOWNLOAD_CONFIG_FILE -Destination "$CONFIG_FOLDER\forge-client.toml"
-    Write-Log "forge-client.toml moved to the config folder."
-
     Write-Host "Config update complete!"
-    Write-Log "Config update complete!"
 } else {
     Write-Host "Download failed. Please check the download link and try again."
-    Write-Log "Download failed. The file does not exist at $DOWNLOAD_CONFIG_FILE."
 }
 
 # Ensure shaderpacks folder exists
 if (-Not (Test-Path $SHADERPACKS_FOLDER)) {
     Write-Host "Creating shaderpacks folder..."
-    Write-Log "Creating shaderpacks folder..."
     New-Item -ItemType Directory -Force -Path $SHADERPACKS_FOLDER
-    Write-Log "Shaderpacks folder created."
 }
 
 # Download the shaderpacks zip from Google Drive using Invoke-WebRequest
@@ -152,15 +116,11 @@ if (-Not (Test-Path $DOWNLOAD_SHADERPACKS_FILE)) {
 
 # Check if the download was successful
 if (Test-Path $DOWNLOAD_SHADERPACKS_FILE) {
-    Write-Log "Download file exists."
-    
     # Extract the downloaded archive using 7-Zip to a temporary location
     Write-Host "Extracting new shaderpacks with 7-Zip..."
-    Write-Log "Extracting new shaderpacks with 7-Zip..."
     $7zipPath = "C:\Program Files\7-Zip\7z.exe" # Adjust this path if 7-Zip is installed elsewhere
     $extractCommand = "& `"$7zipPath`" x `"$DOWNLOAD_SHADERPACKS_FILE`" -o`"$TEMP_EXTRACT_PATH`" -y"
     Invoke-Expression $extractCommand
-    Write-Log "Extraction completed."
 
     # Move files from the temporary extraction path to the shaderpacks folder
     Get-ChildItem -Path "$TEMP_EXTRACT_PATH\*" | Move-Item -Destination $SHADERPACKS_FOLDER -Force
@@ -168,23 +128,16 @@ if (Test-Path $DOWNLOAD_SHADERPACKS_FILE) {
 
     # Clean up the downloaded shaderpacks archive file
     Write-Host "Cleaning up..."
-    Write-Log "Cleaning up..."
     Remove-Item $DOWNLOAD_SHADERPACKS_FILE
-    Write-Log "Download file deleted."
-
     Write-Host "Shaderpacks update complete!"
-    Write-Log "Shaderpacks update complete!"
 } else {
     Write-Host "Download failed. Please check the download link and try again."
-    Write-Log "Download failed. The file does not exist at $DOWNLOAD_SHADERPACKS_FILE."
 }
 
 # Ensure resourcepacks folder exists
 if (-Not (Test-Path $RESOURCEPACKS_FOLDER)) {
     Write-Host "Creating resourcepacks folder..."
-    Write-Log "Creating resourcepacks folder..."
     New-Item -ItemType Directory -Force -Path $RESOURCEPACKS_FOLDER
-    Write-Log "Resourcepacks folder created."
 }
 
 # Download the resourcepacks zip from Google Drive using Invoke-WebRequest
@@ -194,15 +147,11 @@ if (-Not (Test-Path $DOWNLOAD_RESOURCEPACKS_FILE)) {
 
 # Check if the download was successful
 if (Test-Path $DOWNLOAD_RESOURCEPACKS_FILE) {
-    Write-Log "Download file exists."
-    
     # Extract the downloaded archive using 7-Zip to a temporary location
     Write-Host "Extracting new resourcepacks with 7-Zip..."
-    Write-Log "Extracting new resourcepacks with 7-Zip..."
     $7zipPath = "C:\Program Files\7-Zip\7z.exe" # Adjust this path if 7-Zip is installed elsewhere
     $extractCommand = "& `"$7zipPath`" x `"$DOWNLOAD_RESOURCEPACKS_FILE`" -o`"$TEMP_EXTRACT_PATH`" -y"
     Invoke-Expression $extractCommand
-    Write-Log "Extraction completed."
 
     # Move files from the temporary extraction path to the resourcepacks folder
     Get-ChildItem -Path "$TEMP_EXTRACT_PATH\*" | Move-Item -Destination $RESOURCEPACKS_FOLDER -Force
@@ -210,15 +159,10 @@ if (Test-Path $DOWNLOAD_RESOURCEPACKS_FILE) {
 
     # Clean up the downloaded resourcepacks archive file
     Write-Host "Cleaning up..."
-    Write-Log "Cleaning up..."
     Remove-Item $DOWNLOAD_RESOURCEPACKS_FILE
-    Write-Log "Download file deleted."
-
     Write-Host "Resourcepacks update complete!"
-    Write-Log "Resourcepacks update complete!"
 } else {
     Write-Host "Download failed. Please check the download link and try again."
-    Write-Log "Download failed. The file does not exist at $DOWNLOAD_RESOURCEPACKS_FILE."
 }
 
 # Download the optionsshaders.txt from Google Drive using Invoke-WebRequest
@@ -228,18 +172,12 @@ if (-Not (Test-Path $DOWNLOAD_OPTIONS_SHADERS_FILE)) {
 
 # Check if the download was successful
 if (Test-Path $DOWNLOAD_OPTIONS_SHADERS_FILE) {
-    Write-Log "Download file exists."
     # Move the downloaded optionsshaders.txt to the Minecraft folder
     Write-Host "Moving optionsshaders.txt to the Minecraft folder..."
-    Write-Log "Moving optionsshaders.txt to the Minecraft folder..."
     Move-Item -Force -Path $DOWNLOAD_OPTIONS_SHADERS_FILE -Destination "$EXTRACT_PATH\optionsshaders.txt"
-    Write-Log "optionsshaders.txt moved to the Minecraft folder."
-
     Write-Host "Options shaders update complete!"
-    Write-Log "Options shaders update complete!"
 } else {
     Write-Host "Download failed. Please check the download link and try again."
-    Write-Log "Download failed. The file does not exist at $DOWNLOAD_OPTIONS_SHADERS_FILE."
 }
 
 # Download the options.txt from Google Drive using Invoke-WebRequest
@@ -249,24 +187,16 @@ if (-Not (Test-Path $DOWNLOAD_OPTIONS_FILE)) {
 
 # Check if the download was successful
 if (Test-Path $DOWNLOAD_OPTIONS_FILE) {
-    Write-Log "Download file exists."
     # Move the downloaded options.txt to the Minecraft folder
     Write-Host "Moving options.txt to the Minecraft folder..."
-    Write-Log "Moving options.txt to the Minecraft folder..."
     Move-Item -Force -Path $DOWNLOAD_OPTIONS_FILE -Destination "$EXTRACT_PATH\options.txt"
-    Write-Log "options.txt moved to the Minecraft folder."
-
     Write-Host "Options update complete!"
-    Write-Log "Options update complete!"
 } else {
     Write-Host "Download failed. Please check the download link and try again."
-    Write-Log "Download failed. The file does not exist at $DOWNLOAD_OPTIONS_FILE."
 }
 
 # Restore original execution policy
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy $originalExecutionPolicy -Force
-Write-Log "Restored original execution policy to $originalExecutionPolicy."
 
 Write-Host "Update process complete!"
-Write-Log "Update process complete!"
 Pause

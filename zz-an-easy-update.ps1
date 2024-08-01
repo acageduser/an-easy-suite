87c4daf95c58f64e9a3056a6a9376c8411bde427
Ryan Livinghouse <132973297+acageduser@users.noreply.github.com>
2024-08-01 14:18:38 +0900

    Update zz-an-easy-update.ps1

--- zz-an-easy-update.ps1
+++ zz-an-easy-update.ps1
@@ -15,12 +15,12 @@ function Write-Log {
 # Ensure Minecraft is closed before running this script
 Write-Host ""
 Write-Host "Before running this script:"
-Write-Host "	- Close Minecraft."
+Write-Host "    - Close Minecraft."
 Write-Host ""
-Write-Host "	- Verify that the following are installed on your PC:"
-Write-Host "		- Python"
-Write-Host "		- PIP"
-Write-Host "		- 7-zip"
+Write-Host "    - Verify that the following are installed on your PC:"
+Write-Host "        - Python"
+Write-Host "        - PIP"
+Write-Host "        - 7-zip"
 Write-Host ""
 
 # Execution policy change
@@ -41,7 +41,7 @@ $MODS_FOLDER = "$env:APPDATA\.minecraft\mods"
 $CONFIG_FOLDER = "$env:APPDATA\.minecraft\config"
 $SHADERPACKS_FOLDER = "$env:APPDATA\.minecraft\shaderpacks"
 $RESOURCEPACKS_FOLDER = "$env:APPDATA\.minecraft\resourcepacks"
-$EXTRACT_PATH = "$env:APPDATA\.minecraft"
+$TEMP_EXTRACT_PATH = "$env:TEMP\minecraft_temp_extract"
 $DOWNLOAD_MODS_FILE = "$env:TEMP\mods.zip"
 $DOWNLOAD_CONFIG_FILE = "$env:TEMP\forge-client.toml"
 $DOWNLOAD_SHADERPACKS_FILE = "$env:TEMP\shaderpacks.zip"
@@ -49,36 +49,27 @@ $DOWNLOAD_RESOURCEPACKS_FILE = "$env:TEMP\resourcepacks.zip"
 $DOWNLOAD_OPTIONS_SHADERS_FILE = "$env:TEMP\optionsshaders.txt"
 $DOWNLOAD_OPTIONS_FILE = "$env:TEMP\options.txt"
 
-# Ensure gdown is installed
-Write-Host "Checking if gdown is installed..."
-Write-Log "Checking if gdown is installed..."
-
-try {
-    $pythonVersion = & "python" --version 2>&1
-    Write-Log "Python is installed: $pythonVersion"
-} catch {
-    Write-Host "Python is not installed. Please install Python from: https://www.python.org/downloads/release/python-3120/"
-    Write-Log "Python is not installed."
-    exit 1
+# Ensure the temporary extract path exists
+if (-Not (Test-Path $TEMP_EXTRACT_PATH)) {
+    New-Item -ItemType Directory -Force -Path $TEMP_EXTRACT_PATH | Out-Null
 }
 
-try {
-    $pipVersion = & "pip" --version 2>&1
-    Write-Log "pip is installed: $pipVersion"
-} catch {
-    Write-Host "pip is not installed. Please install pip."
-    Write-Log "pip is not installed."
-    exit 1
-}
-
-try {
-    $gdownVersion = & "gdown" --version 2>&1
-    Write-Log "gdown is already installed: $gdownVersion"
-} catch {
-    Write-Host "gdown is not installed. Installing gdown..."
-    Write-Log "gdown is not installed. Installing gdown..."
-    & "pip" install gdown
-    Write-Log "gdown installed successfully."
+# Function to download files
+function Download-File {
+    param (
+        [string]$url,
+        [string]$output
+    )
+    Write-Host "Downloading $output..."
+    Write-Log "Downloading $output..."
+    try {
+        Invoke-WebRequest -Uri $url -OutFile $output
+        Write-Log "Downloaded $output successfully."
+    } catch {
+        Write-Host "Download failed for $output. Please check the download link and try again."
+        Write-Log "Download failed for $output: $_"
+        exit 1
+    }
 }
 
 # Delete the local mods folder before downloading
@@ -89,37 +80,24 @@ if (Test-Path $MODS_FOLDER) {
     Write-Log "Existing mods folder deleted."
 }
 
-
-# Download the mods zip from Google Drive using gdown if it hasn't been downloaded already
+# Download the mods zip from Google Drive using Invoke-WebRequest
 if (-Not (Test-Path $DOWNLOAD_MODS_FILE)) {
-    Write-Host "Downloading mods archive using gdown..."
-    Write-Log "Downloading mods archive using gdown..."
-
-    try {
-        $gdownCommand = "gdown $GDRIVE_MODS_URL -O $DOWNLOAD_MODS_FILE"
-        Write-Log "Running command: $gdownCommand"
-        & "python" -c "import gdown; gdown.download('$GDRIVE_MODS_URL', r'$DOWNLOAD_MODS_FILE', quiet=False)"
-        Write-Log "Download completed."
-    } catch {
-        Write-Host "Download failed. Please check the download link and try again."
-        Write-Log "Download failed: $_"
-        exit 1
-    }
+    Download-File -url $GDRIVE_MODS_URL -output $DOWNLOAD_MODS_FILE
 }
 
 # Check if the download was successful
 if (Test-Path $DOWNLOAD_MODS_FILE) {
     Write-Log "Download file exists."
     
-    # Step 3: Extract the downloaded archive using 7-Zip
+    # Extract the downloaded archive using 7-Zip
     Write-Host "Extracting new mods with 7-Zip..."
     Write-Log "Extracting new mods with 7-Zip..."
     $7zipPath = "C:\Program Files\7-Zip\7z.exe" # Adjust this path if 7-Zip is installed elsewhere
-    $extractCommand = "& `"$7zipPath`" x `"$DOWNLOAD_MODS_FILE`" -o`"$EXTRACT_PATH`" -y"
+    $extractCommand = "& `"$7zipPath`" x `"$DOWNLOAD_MODS_FILE`" -o`"$MODS_FOLDER`" -y"
     Invoke-Expression $extractCommand
     Write-Log "Extraction completed."
 
-    # Step 4: Clean up the downloaded archive file
+    # Clean up the downloaded archive file
     Write-Host "Cleaning up..."
     Write-Log "Cleaning up..."
     Remove-Item $DOWNLOAD_MODS_FILE
@@ -132,21 +110,9 @@ if (Test-Path $DOWNLOAD_MODS_FILE) {
     Write-Log "Download failed. The file does not exist at $DOWNLOAD_MODS_FILE."
 }
 
-# Download the forge-client.toml from Google Drive using gdown if it hasn't been downloaded already
+# Download the forge-client.toml from Google Drive using Invoke-WebRequest
 if (-Not (Test-Path $DOWNLOAD_CONFIG_FILE)) {
-    Write-Host "Downloading forge-client.toml using gdown..."
-    Write-Log "Downloading forge-client.toml using gdown..."
-
-    try {
-        $gdownCommand = "gdown $GDRIVE_CONFIG_URL -O $DOWNLOAD_CONFIG_FILE"
-        Write-Log "Running command: $gdownCommand"
-        & "python" -c "import gdown; gdown.download('$GDRIVE_CONFIG_URL', r'$DOWNLOAD_CONFIG_FILE', quiet=False)"
-        Write-Log "Download completed."
-    } catch {
-        Write-Host "Download failed. Please check the download link and try again."
-        Write-Log "Download failed: $_"
-        exit 1
-    }
+    Download-File -url $GDRIVE_CONFIG_URL -output $DOWNLOAD_CONFIG_FILE
 }
 
 # Check if the download was successful
@@ -158,7 +124,7 @@ if (Test-Path $DOWNLOAD_CONFIG_FILE) {
     Remove-Item -Force "$CONFIG_FOLDER\forge-client.toml"
     Write-Log "Existing forge-client.toml deleted."
 
-    # Step 7: Move the downloaded forge-client.toml to the config folder
+    # Move the downloaded forge-client.toml to the config folder
     Write-Host "Moving new forge-client.toml to the config folder..."
     Write-Log "Moving new forge-client.toml to the config folder..."
     Move-Item -Force -Path $DOWNLOAD_CONFIG_FILE -Destination "$CONFIG_FOLDER\forge-client.toml"
@@ -179,34 +145,26 @@ if (-Not (Test-Path $SHADERPACKS_FOLDER)) {
     Write-Log "Shaderpacks folder created."
 }
 
-# Download the shaderpacks zip from Google Drive using gdown if it hasn't been downloaded already
+# Download the shaderpacks zip from Google Drive using Invoke-WebRequest
 if (-Not (Test-Path $DOWNLOAD_SHADERPACKS_FILE)) {
-    Write-Host "Downloading shaderpacks archive using gdown..."
-    Write-Log "Downloading shaderpacks archive using gdown..."
-
-    try {
-        $gdownCommand = "gdown $GDRIVE_SHADERPACKS_URL -O $DOWNLOAD_SHADERPACKS_FILE"
-        Write-Log "Running command: $gdownCommand"
-        & "python" -c "import gdown; gdown.download('$GDRIVE_SHADERPACKS_URL', r'$DOWNLOAD_SHADERPACKS_FILE', quiet=False)"
-        Write-Log "Download completed."
-    } catch {
-        Write-Host "Download failed. Please check the download link and try again."
-        Write-Log "Download failed: $_"
-        exit 1
-    }
+    Download-File -url $GDRIVE_SHADERPACKS_URL -output $DOWNLOAD_SHADERPACKS_FILE
 }
 
 # Check if the download was successful
 if (Test-Path $DOWNLOAD_SHADERPACKS_FILE) {
     Write-Log "Download file exists."
     
-    # Step 3: Extract the downloaded archive using 7-Zip
+    # Extract the downloaded archive using 7-Zip to a temporary location
     Write-Host "Extracting new shaderpacks with 7-Zip..."
     Write-Log "Extracting new shaderpacks with 7-Zip..."
     $7zipPath = "C:\Program Files\7-Zip\7z.exe" # Adjust this path if 7-Zip is installed elsewhere
-    $extractCommand = "& `"$7zipPath`" x `"$DOWNLOAD_SHADERPACKS_FILE`" -o`"$SHADERPACKS_FOLDER`" -y"
+    $extractCommand = "& `"$7zipPath`" x `"$DOWNLOAD_SHADERPACKS_FILE`" -o`"$TEMP_EXTRACT_PATH`" -y"
     Invoke-Expression $extractCommand
     Write-Log "Extraction completed."
+
+    # Move files from the temporary extraction path to the shaderpacks folder
+    Get-ChildItem -Path "$TEMP_EXTRACT_PATH\*" | Move-Item -Destination $SHADERPACKS_FOLDER -Force
+    Remove-Item -Recurse -Force $TEMP_EXTRACT_PATH
 
     # Clean up the downloaded shaderpacks archive file
     Write-Host "Cleaning up..."
@@ -229,36 +187,28 @@ if (-Not (Test-Path $RESOURCEPACKS_FOLDER)) {
     Write-Log "Resourcepacks folder created."
 }
 
-# Download the resourcepacks zip from Google Drive using gdown if it hasn't been downloaded already
+# Download the resourcepacks zip from Google Drive using Invoke-WebRequest
 if (-Not (Test-Path $DOWNLOAD_RESOURCEPACKS_FILE)) {
-    Write-Host "Downloading resourcepacks archive using gdown..."
-    Write-Log "Downloading resourcepacks archive using gdown..."
-
-    try {
-        $gdownCommand = "gdown $GDRIVE_RESOURCEPACKS_URL -O $DOWNLOAD_RESOURCEPACKS_FILE"
-        Write-Log "Running command: $gdownCommand"
-        & "python" -c "import gdown; gdown.download('$GDRIVE_RESOURCEPACKS_URL', r'$DOWNLOAD_RESOURCEPACKS_FILE', quiet=False)"
-        Write-Log "Download completed."
-    } catch {
-        Write-Host "Download failed. Please check the download link and try again."
-        Write-Log "Download failed: $_"
-        exit 1
-    }
+    Download-File -url $GDRIVE_RESOURCEPACKS_URL -output $DOWNLOAD_RESOURCEPACKS_FILE
 }
 
 # Check if the download was successful
 if (Test-Path $DOWNLOAD_RESOURCEPACKS_FILE) {
     Write-Log "Download file exists."
     
-    # Step 3: Extract the downloaded archive using 7-Zip
+    # Extract the downloaded archive using 7-Zip to a temporary location
     Write-Host "Extracting new resourcepacks with 7-Zip..."
     Write-Log "Extracting new resourcepacks with 7-Zip..."
     $7zipPath = "C:\Program Files\7-Zip\7z.exe" # Adjust this path if 7-Zip is installed elsewhere
-    $extractCommand = "& `"$7zipPath`" x `"$DOWNLOAD_RESOURCEPACKS_FILE`" -o`"$RESOURCEPACKS_FOLDER`" -y"
+    $extractCommand = "& `"$7zipPath`" x `"$DOWNLOAD_RESOURCEPACKS_FILE`" -o`"$TEMP_EXTRACT_PATH`" -y"
     Invoke-Expression $extractCommand
     Write-Log "Extraction completed."
 
-    # Step 4: Clean up the downloaded archive file
+    # Move files from the temporary extraction path to the resourcepacks folder
+    Get-ChildItem -Path "$TEMP_EXTRACT_PATH\*" | Move-Item -Destination $RESOURCEPACKS_FOLDER -Force
+    Remove-Item -Recurse -Force $TEMP_EXTRACT_PATH
+
+    # Clean up the downloaded resourcepacks archive file
     Write-Host "Cleaning up..."
     Write-Log "Cleaning up..."
     Remove-Item $DOWNLOAD_RESOURCEPACKS_FILE
@@ -271,21 +221,9 @@ if (Test-Path $DOWNLOAD_RESOURCEPACKS_FILE) {
     Write-Log "Download failed. The file does not exist at $DOWNLOAD_RESOURCEPACKS_FILE."
 }
 
-# Download the optionsshaders.txt from Google Drive using gdown if it hasn't been downloaded already
+# Download the optionsshaders.txt from Google Drive using Invoke-WebRequest
 if (-Not (Test-Path $DOWNLOAD_OPTIONS_SHADERS_FILE)) {
-    Write-Host "Downloading optionsshaders.txt using gdown..."
-    Write-Log "Downloading optionsshaders.txt using gdown..."
-
-    try {
-        $gdownCommand = "gdown $GDRIVE_OPTIONS_SHADERS_URL -O $DOWNLOAD_OPTIONS_SHADERS_FILE"
-        Write-Log "Running command: $gdownCommand"
-        & "python" -c "import gdown; gdown.download('$GDRIVE_OPTIONS_SHADERS_URL', r'$DOWNLOAD_OPTIONS_SHADERS_FILE', quiet=False)"
-        Write-Log "Download completed."
-    } catch {
-        Write-Host "Download failed. Please check the download link and try again."
-        Write-Log "Download failed: $_"
-        exit 1
-    }
+    Download-File -url $GDRIVE_OPTIONS_SHADERS_URL -output $DOWNLOAD_OPTIONS_SHADERS_FILE
 }
 
 # Check if the download was successful
@@ -304,21 +242,9 @@ if (Test-Path $DOWNLOAD_OPTIONS_SHADERS_FILE) {
     Write-Log "Download failed. The file does not exist at $DOWNLOAD_OPTIONS_SHADERS_FILE."
 }
 
-# Download the options.txt from Google Drive using gdown if it hasn't been downloaded already
+# Download the options.txt from Google Drive using Invoke-WebRequest
 if (-Not (Test-Path $DOWNLOAD_OPTIONS_FILE)) {
-    Write-Host "Downloading options.txt using gdown..."
-    Write-Log "Downloading options.txt using gdown..."
-
-    try {
-        $gdownCommand = "gdown $GDRIVE_OPTIONS_URL -O $DOWNLOAD_OPTIONS_FILE"
-        Write-Log "Running command: $gdownCommand"
-        & "python" -c "import gdown; gdown.download('$GDRIVE_OPTIONS_URL', r'$DOWNLOAD_OPTIONS_FILE', quiet=False)"
-        Write-Log "Download completed."
-    } catch {
-        Write-Host "Download failed. Please check the download link and try again."
-        Write-Log "Download failed: $_"
-        exit 1
-    }
+    Download-File -url $GDRIVE_OPTIONS_URL -output $DOWNLOAD_OPTIONS_FILE
 }
 
 # Check if the download was successful
